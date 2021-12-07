1eef61d565794ea4a834cb99a0f3d0c4dc656943
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-12-07 20:40:40 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -15,14 +15,14 @@ const Config = require('../config');
 if (Config.WORKTYPE == 'private') {
 CBot.addCommand({pattern: 'alive', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
-	await message.client.sendMessage(message.jid, fs.readFileSync('./media/cyberbot.mp3'), MessageType.audio, {mimetype: 'audio/mp4', ptt:true}, {quoted: message.data})
+	await message.client.sendMessage(message.jid, fs.readFileSync('./media/cyberbot.mp3'), MessageType.audio, {mimetype: 'audio/mp4', ptt:true};
 }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 CBot.addCommand({pattern: 'alive', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 	
-    await message.client.sendMessage(message.jid, fs.readFileSync('./media/cyberbot.mp3'), MessageType.audio, {mimetype: 'audio/mp4', ptt:true}, {quoted: message.data})
+    await message.client.sendMessage(message.jid, fs.readFileSync('./media/cyberbot.mp3'), MessageType.audio, {mimetype: 'audio/mp4', ptt:true}; 
 }));
 
 }
