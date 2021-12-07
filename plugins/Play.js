3d2601e4b25d8466a7d116041927e6867571ce1e
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-12-07 22:51:59 +0530

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -7,7 +7,9 @@ const axios = require('axios')
 const Axios = require('axios')
 
 
-Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+let whb = Config.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'song ?(.*)', fromMe: whb, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 	
 	await message.client.sendMessage(message.jid, '*Im Searching your song  👸🏼 ➾ 🔎*' , MessageType.text, { quoted: message.data });
 	
