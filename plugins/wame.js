5edd3149646dcd14794ef8391615a9faa69f214d
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-12-08 21:23:10 +0530

    Update wame.js

--- plugins/wame.js
+++ plugins/wame.js
@@ -1,5 +1,5 @@
 const Asena = require('../events');
-const {MessageType} = require('@aqua-snake/cyber-bot-web');
+const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const fs = require('fs');
 
