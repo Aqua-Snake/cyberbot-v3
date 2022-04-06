c02811a93c888790016d5f2e1523e3f98ce19005
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2022-04-06 13:24:54 +0530

    Updateted Alive message

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -9,31 +9,58 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
+let whb = Config.WORKTYPE == 'public' ? false : true
 
-if (Config.WORKTYPE == 'private') {
+CBot.addCommand({pattern: 'alive ?(.*)', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 
-    CBot.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+    var logo = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
+    var PIC = Buffer.from(logo.data)
+
+    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.image, { thumbnail: PIC })
+
+// send a buttons message
+var BUTTHANDLE = '';
+    if (/\[(\W*)\]/.test(Build.HANDLERS)) {
+        BUTTHANDLE = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+    } else {
+        BUTTHANDLE = '.';
+    }
+
+    const buttons = [
+        {buttonId: BUTTHANDLE + 'version', buttonText: {displayText: "Version" }, type: 1},
+        {buttonId: BUTTHANDLE + 'syssts', buttonText: {displayText: "System Status" }, type: 1}
+    ]
+      
+      const buttonMessage = {
+          contentText: '```'+Config.NBOT+'\n\n```'+Config.ALIVEMSG+'\n',
+          footerText: 'ᴄʏʙᴇʀ ʙᴏᴛ © ɢʟᴏʙᴀʟ ᴇᴅɪᴛɪᴏɴ',
+          buttons: buttons,
+         headerType: 4,
+        imageMessage: media.message.imageMessage    
+    }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
+}))
+
+
+
+
+    CBot.addCommand({pattern: 'syssts', fromMe: whb, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
         const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
         await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
+            '```' + child + '```', MessageType.text, {quoted: message.data});
     }));
-}
-else if (Config.WORKTYPE == 'public') {
-    CBot.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
+
+    CBot.addCommand({pattern: 'version', fromMe: whb, desc: Lang.SYSD_DESC}, (async (message, match) => {
+
+        await message.client.sendMessage(message.jid, 
+        `*🧬 Cyber Bot Version 🧬*\n\n` + 
+        '```Installed version :```\n' +
+        Config.version + 
+        `\n\nCheck official website for more updates`
+   , MessageType.text, {quoted: message.data});
     
-    CBot.addCommand({pattern: 'psysd', fromMe: true, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
+}));
 
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-}
