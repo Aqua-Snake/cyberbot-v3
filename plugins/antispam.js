1748ddf399e011c489a3530ced5103511b785b0a
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2022-04-06 19:44:26 +0530

    Update antispam.js

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -7,12 +7,21 @@ Cyber Bot - Aqua Snake
 */
 
 const CBot = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const fs = require('fs');
 
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {
+        if (member.jid.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
 
 CBot.addCommand({pattern: 'antispam ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
-  await message.client.sendMessage(message.jid, '.mute', MessageType.text);
+  await message.client.groupSettingChange(message.jid, GroupSettingChange.messageSend, true);
   await message.client.sendMessage(message.jid, 'Activating Antispam Mode', MessageType.text);
   await message.client.sendMessage(message.jid, 'AɳƚιSραɱ \nRιზზσɳ \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n A\n ɳ\n ƚ\n ι\n S\n ρ\n α\n ɱ\n\n R\n ι\n ზ\n ზ\n σ\n ɳ\n\n 🄲🅈🄱🄴🅁 \n🄱🄾🅃\n ', MessageType.text);
   await message.client.sendMessage(message.jid, 'AɳƚιSραɱ \nRιზზσɳ \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n A\n ɳ\n ƚ\n ι\n S\n ρ\n α\n ɱ\n\n R\n ι\n ზ\n ზ\n σ\n ɳ\n\n 🄲🅈🄱🄴🅁 \n🄱🄾🅃\n ', MessageType.text);
@@ -21,5 +30,16 @@ CBot.addCommand({pattern: 'antispam ?(.*)', fromMe: true, dontAddCommandList: tr
   await message.client.sendMessage(message.jid, 'AɳƚιSραɱ \nRιზზσɳ \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n A\n ɳ\n ƚ\n ι\n S\n ρ\n α\n ɱ\n\n R\n ι\n ზ\n ზ\n σ\n ɳ\n\n 🄲🅈🄱🄴🅁 \n🄱🄾🅃\n ', MessageType.text);
   await message.client.sendMessage(message.jid, 'Antispam Mode This is useful for prevent from spams', MessageType.text);
   await message.client.sendMessage(message.jid, '*All group members must clear your chats*', MessageType.text);
+  grup = await message.client.groupMetadata(message.jid);
+            var jids = [];
+            mesaj = '*All group members must clear your chats*';
+            grup['participants'].map(
+                async (uye) => {
+                    mesaj += '@' + uye.id.split('@')[0] + ' ';
+                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+                }
+            );
+            await message.client.sendMessage(message.jid,`${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        }
 
 }));
